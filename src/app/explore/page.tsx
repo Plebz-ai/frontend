fec424b2717a9d36a1387bf038b28742a92806cb
hardikjumnani <sauhard74best@gmail.com>
2025-04-26 21:27:39 +0530

    update: search bar positioning

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -575,7 +575,7 @@ export default function ExplorePage() {
               {menuOpen ? <X className="w-5 h-5" /> : <Menu className="w-5 h-5" />}
             </button>
             
-            <div className="relative flex-1 max-w-xl mx-4">
+            <div className="relative flex-1 max-w-xl mx-auto">
               <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
                 <Search className="h-4 w-4 text-gray-400" />
               </div>
